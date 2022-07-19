9461d385cb40edfc5c0cb8c500763d598fc3a2fa
Jimmy Ma <ma_yijun2016@163.com>
2022-07-19 22:46:15 +0800

    some changes.

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     @RequestMapping("/test")
     public String test() {
-        return "Hello World!";
+        return "Hello World!Hello Jinkins!!!";
     }
 }
